1f8ff4de75de75b6a170be465aaf8296f0828976
Daniel Rispler <danielrispler@gmail.com>
2026-04-02 20:36:49 +0300

    chore: update default API base URL and increment build version to 1.0.0+5

--- apps/mobile/lib/core/config/api_config.dart
+++ apps/mobile/lib/core/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String baseUrlDefineKey = 'WISHIZ_API_BASE_URL';
-  static const String defaultBaseUrl = 'http://34.78.227.223:8080';
+  static const String defaultBaseUrl = 'http://34.78.227.223';
 
   static String? get baseUrl {
     const configuredValue = String.fromEnvironment(baseUrlDefineKey);
